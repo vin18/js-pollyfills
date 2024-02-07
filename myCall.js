9d94fab7cdd951cec8e96c0ccae471e8794cd677
Vinit Raut <31928236+vin18@users.noreply.github.com>
2024-02-07 10:16:44 -0800

    Update myCall.js

--- myCall.js
+++ myCall.js
@@ -1,4 +1,8 @@
 function myCall(scope, ...args) {
+  if (typeof this !== 'function') {
+    throw new TypeError(`${this} is not a function`);
+  }
+  
   scope.fn = this;
   return scope.fn(...args);
 }
